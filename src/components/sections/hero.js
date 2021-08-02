51b3ee91bbe6495fbb4329c8ed2589cd0054e646
Justin Minyard <jminyard504@gmail.com>
2021-08-02 15:54:11 -0400

    Adds honeycomb images; works on post-hero sections

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -4,6 +4,7 @@ import heroStyles from '../../styles/hero.module.scss';
 // Components
 import Nav from './nav';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Hero(props) {
 
@@ -20,11 +21,17 @@ export default function Hero(props) {
 
 					<h1>Justin Minyard</h1>
 					<div className={ heroStyles.titleAccentWrap }>
-
+						<Image src='/img/hero-accent.svg' width={ 1000 } height={ 50 } />
 					</div>
 					<h2>full-stack web developer</h2>
 
 					<a className={ heroStyles.btn } href="#work">View work</a>
+
+				</div>
+
+				<div className={ heroStyles.scrollArrowWrap }>
+
+					<Image src='/img/scroll-arrow.svg' width={ 10 } height={ 71 } />
 
 				</div>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,9 @@
+// Styles
+import sectionStyles from '../styles/sections.module.scss';
+
+// Components
 import Head from 'next/head'
+import Image from 'next/image';
 import Hero from '../components/sections/hero';
 import About from '../components/sections/about';
 import Skills from '../components/sections/skills';
@@ -9,13 +14,24 @@ export default function Home() {
 	return (
 		<>
 
-		<Head>
-			<title>Justin Minyard | Full-Stack Developer</title>
-		</Head>
-		<Hero/>
-		<About />
-		<Skills />
-		<Work />
+			<Head>
+				<title>Justin Minyard | Full-Stack Developer</title>
+			</Head>
+			<Hero/>
+
+			<section className={ sectionStyles.section2 }>
+				<div className={ sectionStyles.honeycomb1 }>
+					<Image src='/img/honeycomb_1.svg' width={ 600 } height={ 600 } />
+				</div>
+
+				<div className={ sectionStyles.honeycomb2 }>
+					<Image src='/img/honeycomb_2.svg' width={ 600 } height={ 600 } />
+				</div>
+				<About />
+				<Skills />
+			</section>
+
+			<Work />
 
 		</>
 
