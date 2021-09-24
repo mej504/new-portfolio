9e722714091766167f1eee225544e294aa8b8275
Justin Minyard <jminyard504@gmail.com>
2021-09-24 12:03:40 -0400

    Updates about section; changes references to Sass to SCSS

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -12,17 +12,11 @@ export default function About() {
 
 					<img src="/img/Justin_Medium.jpg" />
 
-					{
-					/*
-						<Image src="/img/CC2.png" width={ 600 } height={ 600 } objectFit='cover' />
-					*/
-					}
-
 				</div>
 
 				<div className={ aboutStyles.aboutCopyContainer }>
 					<h2>Developer. Problem solver. Minimalist.</h2>
-					<p>Hey there! I'm Justin, a freelance full-stack developer based out of Louisville, Kentucky. I specialize in building scalable back-end infrastructures, and I love the challenge of creating, intuitive front-end experiences.</p>
+					<p>Hey there! I'm Justin, a freelance full-stack developer based out of Louisville, Kentucky. I specialize in building Node applications and clean, intuitive front-end experiences from the ground up.</p>
 				</div>
 
 			</div>
