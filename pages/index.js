e7f10bb4234bcf3d11df18269497f7d4e46cf172
Justin Minyard <jminyard504@gmail.com>
2021-10-15 14:32:25 -0400

    Changes page's meta description

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home({ path }) {
 
 			<Head>
 				<title>Justin Minyard | Full-Stack Developer</title>
-				<meta name="description" content="Justin Minyard is a Louisville-based full-stack web developer. He specializes in NodeJS applications." />
+				<meta name="description" content="Justin Minyard is a Louisville-based full-stack web developer. He specializes in building Node.js applications, and has deployed front-ends built with React, Vue and Next." />
 			</Head>
 
 			<Nav location={ path } />
