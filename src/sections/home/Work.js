75a1fdd578fa492eb76e0c668ea53c1985c97a97
Justin Minyard <jminyard504@gmail.com>
2021-10-19 13:16:04 -0400

    Overhauls component structure

--- src/sections/home/Work.js
+++ src/sections/home/Work.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import workDetails from 'lib/work-cards';
 
 import WorkCard from '@/home-components/WorkCard';
@@ -6,48 +5,13 @@ import ButtonWithDropShadow from '@/components/buttons/ButtonWithShadow';
 
 import styles from './styles/work.module.scss';
 
-export default function Work({ btnTarget }) {
-
-	// State
-	const [ deviceSize, setDeviceSize ] = useState(null);
+export default function Work() {
 
 	const btnText = 'More work';
 
-	const handleResize = () => {
-
-		if( window.innerWidth <= 1024 && deviceSize === 'desktop' ) {
-			setDeviceSize('tablet');
-		}
-
-		if( window.innerWidth > 1024 && deviceSize === 'tablet' ) {
-			setDeviceSize('desktop');
-		}
-
-	}
-
-	useEffect(() => {
-
-		// Intention was to function across a broad range of resolutions 
-		// Swapping between two ranges is sufficient for this component
-		if( window.innerWidth <= 1024 ) {
-			setDeviceSize('tablet');
-		} else { 
-			setDeviceSize('desktop');
-		}
-
-		window.addEventListener('resize', handleResize );
-
-		return () => {
-			window.removeEventListener( 'resize', handleResize );
-		}
-
-	}, [ deviceSize ])
-
 	return (
 
-		<section id="work" className={ styles.workContainer }>
-
-			<div className={ styles.bg } />
+		<section id="work" className={ styles.workSectionContainer }>
 
 			<h2 className={ styles.header }>Selected Works</h2>
 
@@ -56,20 +20,20 @@ export default function Work({ btnTarget }) {
 				{workDetails.map( work => {
 					return (
 						<WorkCard
-							deviceSize={ deviceSize }
 							title={ work.name }
 							url={ work.url }
 							description={ work.description }
 							builtWith={ work.builtWith }
 							category={ work.category }
 							imgPath= { work.bgImagePath }
-							key={ work.name }/>
+							key={ work.name }
+						/>
 					)
 				})}
 
 			</div>
 
-			<ButtonWithDropShadow target={ btnTarget } style={{ marginTop: '1em' }} btnText={ btnText } />
+			<ButtonWithDropShadow target='/work' style={{ marginTop: '5em' }} btnText={ btnText } />
 
 		</section>
 
