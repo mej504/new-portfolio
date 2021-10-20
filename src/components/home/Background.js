90359179cdfbb95c1541b442c016f6c8cb72c276
Justin Minyard <jminyard504@gmail.com>
2021-10-20 18:09:36 -0400

    Adds placeholder image while main video loads in background

--- src/components/home/Background.js
+++ src/components/home/Background.js
@@ -1,19 +1,54 @@
+import { useRef, useEffect, useState } from 'react';
+
 import styles from './styles/background.module.scss';
+
+import Image from 'next/image';
 
 const Background = () => {
 
+	const videoLoaded = useRef(false);
+	const container = useRef(null);
+	const placeholder = useRef(null);
+
+	const loadVideo = () => {
+
+		//Elements
+		const vidEl = document.createElement('video');
+
+		vidEl.src = '/mov/bg-lines.m4v';
+		vidEl.load();
+		vidEl.muted = true;
+		vidEl.loop = true;
+		vidEl.playsInline = true;
+		vidEl.autoplay = true;
+		vidEl.setAttribute('type','video/mp4');
+
+		vidEl.addEventListener('loadeddata', (e) => {
+
+			if( vidEl.readyState === 4 ) {
+
+				videoLoaded.current = true;
+				placeholder.current.remove();
+				container.current.append(vidEl);
+				vidEl.play();
+			}
+
+		})
+
+	};
+
+	useEffect(() => {
+
+		if( !videoLoaded.current ) loadVideo();
+
+	}, [])
+
 	return (
 
-		<div className={ styles.backgroundVideo }>
-
-			<video
-				src="/mov/bg-lines.m4v"
-				playsInline
-				loop
-				autoPlay
-				muted
-			/>
-
+		<div ref={ container } className={ styles.backgroundVideo }>
+			<div ref={placeholder} className={ styles.placeholderImgContainer }>
+				<Image src="/img/bg-video-still.png" priority="preload" layout="fill" objectFit="cover" />
+			</div>
 		</div>
 	)
 
