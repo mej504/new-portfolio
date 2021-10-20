0bfee5fef589b50b5b54a6cb6ba6a9d0a86aee9d
Justin Minyard <jminyard504@gmail.com>
2021-10-20 14:13:18 -0400

    Major cleanup and refactoring

--- src/components/home/BackToTop.js
+++ src/components/home/BackToTop.js
@@ -2,58 +2,59 @@
 import { useRef, useState, useEffect } from 'react';
 import styles from './styles/back-to-top.module.scss';
 
-export default function BackToTopArrow({ bottomLimit, formBtnPosition, heroSectionRef, location }) {
+export default function BackToTopArrow({ heroSectionRef }) {
 
-	// Constants
-	const STARTING_Y = 15;
+	// CONSTANTS
+	const animationTriggersDefaults = {
+		shouldAnimateIn:false,
+		shouldAnimateOut:false
+	}
 
-	// State
-	const [ buttonState, setButtonState ] = useState({
-		visible: null,
-		positionProp: '',
-		location: null,
-		yPosition: STARTING_Y
-	});
-	
-	// Refs
-	const bottomLimitReached = useRef(null);
-	const heroThresholdReached = useRef(null);
+	// STATE
+	const [ buttonIsVisible, setButtonIsVisible ] = useState(false);
 
+	// REFS
 	const heroCutoff = useRef(null);
-	const bottomCutoff = useRef(null);
 
-	const elPositionProp = useRef(null);
-	const scrollYPos = useRef(null);
-	const thisBtn = useRef(null);
-	const isVisible = useRef(false);
+	// Popping these in a ref to ensure the triggers persist through the re-render
+	const buttonAnimationTriggers = useRef({
+		...animationTriggersDefaults
+	})
+
+	/**
+	 * Handles the back-to-top button state based on the user's current location on the page
+	 * 
+	 * @returns void
+	 * 
+	 */
 
 	const handleScroll = () => {
 
 		let { scrollY } = window
-		let heroPoint = heroCutoff.current;
-		let bottomPoint = bottomCutoff.current;
-		scrollYPos.current = scrollY;
+		let { current: heroPoint } = heroCutoff;
 
-		// Update button if in top region
-		if( scrollYPos.current < heroPoint && ( buttonState.visible || buttonState.visible === null ) ) {
+		// Animate the button out if we pass the heroPoint threshold
+		if( scrollY < heroPoint && buttonIsVisible ) {
 			
-			setButtonState({
-				...buttonState,
-				visible:false
-			})
+			buttonAnimationTriggers.current = {
+				shouldAnimateOut:true,
+				shouldAnimateIn:false,
+			}
+
+			setButtonIsVisible(false);
 
 			return;
 		}
 
-		// Update button if in middle region
-		if( ( scrollYPos.current >= heroPoint && scrollYPos.current < bottomPoint ) && ( !buttonState.visible || buttonState.positionProp === 'absolute' ) ) {
+		// Animate the button in if we pass the heroPoint threshold
+		if( ( scrollY >= heroPoint ) && !buttonIsVisible ) {
 
-			setButtonState({
-				visible:true,
-				positionProp:'fixed',
-				location:'bottom',
-				yPosition:STARTING_Y
-			})
+			buttonAnimationTriggers.current = {
+				shouldAnimateOut:false,
+				shouldAnimateIn:true,
+			}
+
+			setButtonIsVisible(true);
 
 			return;
 
@@ -61,13 +62,12 @@ export default function BackToTopArrow({ bottomLimit, formBtnPosition, heroSecti
 
 	}
 
-	// Update current y scroll position once mounted
 	useEffect(() => {
 
+		// Grabs the point at which the back-to-top button must update
 		heroCutoff.current = heroSectionRef.current.clientHeight / 2;
-		bottomCutoff.current = formBtnPosition.current;
 
-		// Kind of a misnomer for the function name  in this context, but oh well
+		// Sets initial state of button
 		handleScroll();
 
 		window.addEventListener('scroll', handleScroll);
@@ -76,52 +76,23 @@ export default function BackToTopArrow({ bottomLimit, formBtnPosition, heroSecti
 			window.removeEventListener('scroll', handleScroll);
 		}
 
-	}, [ buttonState ]);
+	}, [ buttonIsVisible ] );
 
 
 	return (
-		<>
-			<div
-				onClick={ () => window.scrollTo({top:0}) }
-				ref={thisBtn}
-				className={`button-wrap ${ buttonState.visible === true ? styles.animateIn : styles.animateOut }` }>
 
-				<svg className={ styles.backToTopArrow } viewBox="0 0 75.04 75.04">
-					<circle className={ styles.buttonEllipse } cx="37.52" cy="37.52" r="37.52"/>
-					<path className={ styles.arrowTip } d="M37.67,21.23,46.86,35H28.48Z" transform="translate(0.04 0.21)"/>
-					<rect className={ styles.arrowBody } x="36.56" y="35.22" width="2.3" height="17.99"/>
-				</svg>
+		<div
+			onClick={ () => window.scrollTo({top:0}) }
+			className={`${styles.buttonWrap} ${ buttonAnimationTriggers.current.shouldAnimateIn ? styles.animateIn : '' } ${ buttonAnimationTriggers.current.shouldAnimateOut ? styles.animateOut : ''}` }>
 
-			</div>
+			<svg className={ styles.backToTopArrow } viewBox="0 0 75.04 75.04">
+				<circle className={ styles.buttonEllipse } cx="37.52" cy="37.52" r="37.52"/>
+				<path className={ styles.arrowTip } d="M37.67,21.23,46.86,35H28.48Z" transform="translate(0.04 0.21)"/>
+				<rect className={ styles.arrowBody } x="36.56" y="35.22" width="2.3" height="17.99"/>
+			</svg>
 
-			<style jsx>{`
+		</div>
 
-				.button-wrap {
-					position:${ buttonState.positionProp };
-					right:15px;
-					${ buttonState.location }:${ buttonState.yPosition }px;
-					margin:3em;
-					border-radius:50%;
-					max-height:75px;
-					max-width:75px;
-					cursor:pointer;
-					overflow:hidden;
-					z-index:100;
-					transform:scale(1);
-					transform-origin:center;
-				}
-
-				@media screen and (max-width:760px) {
-					.button-wrap {
-						max-height:50px;
-						max-width:50px;
-						right:-20px;
-					}
-				}
-
-			`}</style>
-
-		</>
 	)
 
 }
