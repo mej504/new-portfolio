91a2a5feb36bec8216cb66e4aa9b011a63c808f0
Justin Minyard <jminyard504@gmail.com>
2021-10-21 06:49:58 -0400

    Promisifies video loader; adds documentation

--- src/components/home/Background.js
+++ src/components/home/Background.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 
 import styles from './styles/background.module.scss';
 
@@ -6,40 +6,68 @@ import Image from 'next/image';
 
 const Background = () => {
 
+	// Refs
 	const videoLoaded = useRef(false);
+	
+	// Element refs
 	const container = useRef(null);
 	const placeholder = useRef(null);
 
-	const loadVideo = () => {
+	const loadVideo = () => new Promise((res, rej) => {
 
 		//Elements
 		const vidEl = document.createElement('video');
 
+		/** VIDEO CONFIG **/
 		vidEl.src = '/mov/bg-lines.m4v';
+
+		// Tell the browser to begin fetching the media the source specified above
 		vidEl.load();
+
+		/**
+		 * * NOTE *
+		 * The video WILL NOT play without muted and autoplay set to true in this way.
+		 * Attempting to set these properties by setAttribute() yields negative results
+		 * 
+		*/
+
+		// Options
 		vidEl.muted = true;
 		vidEl.loop = true;
 		vidEl.playsInline = true;
 		vidEl.autoplay = true;
 		vidEl.setAttribute('type','video/mp4');
 
+		// Just reject if we get an error. The placeholder image isn't ideal, but it does serve
+		// as a decent background image
+		vidEl.addEventListener('error', (e) => {
+			rej();
+		})
+
 		vidEl.addEventListener('loadeddata', (e) => {
 
+			// If the video is ready, update the videoLoaded bool, remove the placeholder
+			// image, add the video to the DOM, then play it
 			if( vidEl.readyState === 4 ) {
-
 				videoLoaded.current = true;
-				placeholder.current.remove();
-				container.current.append(vidEl);
-				vidEl.play();
+				res(vidEl);
 			}
 
 		})
 
-	};
+	});
 
 	useEffect(() => {
 
-		if( !videoLoaded.current ) loadVideo();
+		if( !videoLoaded.current ) {
+			loadVideo().then((vid) => {
+				placeholder.current.remove();
+				container.current.append(vid);
+				vid.play();
+			}).catch((err) => {
+				return;
+			})
+		}
 
 	}, [])
 
